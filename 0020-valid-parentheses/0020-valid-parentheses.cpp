4c8fc768e760165617599092d03b9dd7d5099ca5
Tanvi-Sharma30 <tanvisha309@gmail.com>
2026-07-06 01:48:47 +0530

    Time: 0 ms (100%), Space: 9 MB (37.95%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       stack<char>st;
+       for(int i=0; i<s.size();i++){
+        if(s[i]=='('|| s[i]=='{'|| s[i]=='[') st.push(s[i]);
+        else if(s[i]==')'|| s[i]=='}'|| s[i]==']'){
+           if(st.empty()) return false;
+           char ch = st.top();
+           st.pop();
+           if(ch=='{' && s[i]=='}' || ch=='(' && s[i]==')' || ch=='[' && s[i]==']') continue;
+           else return false;
+        }
+       }
+       
+    return st.empty()?true:false;
+    }
+};
